ec3bd01f246059cc113b0a1fe0e9298ba30e28e7
Tatsuto Yamamoto <laminne33569@approvers.dev>
2021-12-17 01:02:20 +0900

    feat: ユーザー情報更新のエンドポイントの実装

--- repository/users.go
+++ repository/users.go
@@ -8,8 +8,9 @@ import (
 )
 
 type Option struct {
-	Name     string
-	Password string
+	Name     *string
+	Password *string
+	Id       uuid.UUID
 }
 
 func CreateUser(Option Option) (User, bool) {
@@ -19,7 +20,7 @@ func CreateUser(Option Option) (User, bool) {
 	}
 
 	id, _ := uuid.NewUUID()
-	user := User{ID: id, Name: Option.Name, Password: Option.Password}
+	user := User{ID: id, Name: *Option.Name, Password: *Option.Password}
 
 	res := db.Create(&user)
 	if res.Error != nil {
@@ -28,4 +29,51 @@ func CreateUser(Option Option) (User, bool) {
 	}
 
 	return user, true
+}
+func UpdateUser(Option Option) (User, bool) {
+	db, err := gorm.Open(mysql.Open(DB), &gorm.Config{})
+	if err != nil {
+		panic("DB Connection Failed")
+	}
+	var res *gorm.DB
+	var user *User
+	if Option.Name != nil && Option.Password != nil {
+		// どちらもある
+		res = db.Model(&user).Where(User{ID: Option.Id}).Updates(User{Name: *Option.Name, Password: *Option.Password})
+		if res.Error != nil {
+			fmt.Printf("Error: %v", res.Error)
+			return User{}, false
+		}
+		db.First(&user, Option.Id)
+		if res.Error != nil {
+			return User{}, false
+		}
+		return *user, true
+	} else if Option.Password != nil && Option.Name == nil {
+		// Passwordを更新
+		res = db.Model(&user).Where(User{ID: Option.Id}).Updates(User{Password: *Option.Password})
+		if res.Error != nil {
+			fmt.Printf("Error: %v", res.Error)
+			return User{}, false
+		}
+		db.First(&user, Option.Id)
+		if res.Error != nil {
+			return User{}, false
+		}
+		return *user, true
+	} else if Option.Name != nil && Option.Password == nil {
+		res = db.Model(&user).Where(User{ID: Option.Id}).Updates(User{Name: *Option.Name})
+		if res.Error != nil {
+			fmt.Printf("Error: %v", res.Error)
+			return User{}, false
+		}
+		db.First(&user, Option.Id)
+		if res.Error != nil {
+			return User{}, false
+		}
+		return *user, true
+	} else {
+		return User{}, false
+	}
+	return User{}, false
 }
--- routers/users.go
+++ routers/users.go
@@ -1,15 +1,26 @@
 package routers
 
 import (
+	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 	"github.com/pockament/auth/repository"
 	"github.com/pockament/auth/service"
+	"io"
 )
 
 type user struct {
 	Id   uuid.UUID
 	Name string
+}
+type Option struct {
+	Name     string
+	Password string
+}
+
+type update struct {
+	Name     *string
+	Password *string
 }
 
 func CreateUserHandler(c *gin.Context) {
@@ -22,8 +33,8 @@ func CreateUserHandler(c *gin.Context) {
 		})
 	}
 	User, err := service.CreateUser(
-		Option.Name,
-		Option.Password,
+		*Option.Name,
+		*Option.Password,
 	)
 	user := user{
 		Id:   User.ID,
@@ -43,8 +54,72 @@ func CreateUserHandler(c *gin.Context) {
 	}
 }
 
-func UpdateUser(c *gin.Context) {
-	panic("Not Inplemented!")
+func UpdateUserHander(c *gin.Context) {
+	var u update
+	body, _ := io.ReadAll(c.Request.Body)
+	token := c.Request.Header.Get("authorization")
+	if token == "" {
+		c.JSON(400, gin.H{
+			"success": false,
+			"code":    0x0002,
+			"reason":  "token not found",
+		})
+		return
+	}
+
+	err := json.Unmarshal(body, &u)
+	if err != nil {
+		return
+	}
+
+	if u.Name != nil && u.Password != nil {
+		updateUser, err := service.UpdateUser(token, u.Name, u.Password)
+		if err != nil {
+			c.JSON(400, gin.H{
+				"success": false,
+				"code":    0x0002,
+				"reason":  "failed to update your information",
+			})
+		} else {
+			c.JSON(201, gin.H{
+				"success": true,
+				"code":    0x0001,
+				"user":    updateUser,
+			})
+		}
+	} else if u.Name == nil {
+		updateUser, err := service.UpdateUser(token, nil, u.Password)
+		if err != nil {
+			c.JSON(400, gin.H{
+				"success": false,
+				"code":    0x0002,
+				"reason":  "failed to update your information.",
+			})
+		} else {
+			c.JSON(201, gin.H{
+				"success": true,
+				"code":    0x0001,
+				"user":    updateUser,
+			})
+		}
+	} else if u.Password == nil {
+		updateUser, err := service.UpdateUser(token, u.Name, nil)
+		if err != nil {
+			c.JSON(400, gin.H{
+				"success": false,
+				"code":    0x0002,
+				"reason":  "failed to update your information..",
+			})
+		} else {
+			c.JSON(201, gin.H{
+				"success": true,
+				"code":    0x0001,
+				"user":    updateUser,
+			})
+		}
+	} else {
+
+	}
 }
 
 func DeleteUser(c *gin.Context) {
